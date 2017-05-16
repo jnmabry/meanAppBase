4fd06c72c4b78862f971138639c1462bcaccce7b
Joshua Mabry <inkandcapture@gmail.com>
2017-05-16 12:48:57 -0400

    Converts get call to mongoose syntax

--- server/routes/api.js
+++ server/routes/api.js
@@ -19,11 +19,12 @@ router.get('/', (req, res) => {
 // Get all posts
 router.get('/posts', (req, res) => {
   // Get posts from the remote db
-  db.collection('animals').find().toArray(function (err, result) {
-    if (err) throw res.status(500).send(error)
-    res.status(200).json(result);
-    console.log(result)
-    })
+  Post.find({}, function(err, posts) {
+    if (err) throw res.status(500).send(error);
+      // object of all the posts
+      console.log(posts);
+      res.status(200).json(posts);
+  });
 });
 
 // Post a post
@@ -39,21 +40,7 @@ router.post('/posts', (req, res) => {
     // This createdTodoObject is the same one we saved, but after Mongo
     // added its additional properties like _id.
     res.send(createdAnimalObject);
-});
-
-  // Get posts from the remote db using mongo db syntax but not mongoose
-  // This is the long shitty way to add stuff to a database
-
-    // db.collection('animals')
-    //     .insert(animal, {safe:true}, function(err, result) {
-    //         if (err) {
-    //             res.send({'error':'An error has occurred'});
-    //         } else {
-    //             console.log('Success: ' + result);
-    //             res.send(result);
-    //         }
-    //     });
-
+  });
 });
 
 module.exports = router;
